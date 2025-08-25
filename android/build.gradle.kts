d0e896358d5b3cfb5adaedef3438c126cfa83a0c
M Raqib Usman <raqibusman9@gmail.com>
2025-08-25 20:37:47 +0500

    firebase integration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,7 +15,7 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-apply plugin:id("com.google.gms.google-services") version "4.4.3" apply false
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
